b0a9575bf78379f7178fd350f8b07d2c59ac60f6
Derek Hayes <derek2099@gmail.com>
2024-10-10 13:42:11 -0500

    Make checkmarks toggleable

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ const newDialog = document.querySelector("dialog");
 const cancelBtn = document.querySelector("#cancelBtn");
 const submitBtn = document.querySelector("#submitBtn");
 const newBookForm = document.querySelector("form");
+const check = '<svg id="check" fill="#000001" width="50px" height="50px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>checkmark2</title><path d="M28.998 8.531l-2.134-2.134c-0.394-0.393-1.030-0.393-1.423 0l-12.795 12.795-6.086-6.13c-0.393-0.393-1.029-0.393-1.423 0l-2.134 2.134c-0.393 0.394-0.393 1.030 0 1.423l8.924 8.984c0.393 0.393 1.030 0.393 1.423 0l15.648-15.649c0.393-0.392 0.393-1.030 0-1.423z"></path></svg>';
+const x = '<svg id="x" fill="#000000" width="50px" height="50px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>cancel2</title><path d="M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z"></path></svg>';
 
 
 function Book(title, author, numPages, publicationYear, isRead) {
@@ -25,7 +27,6 @@ function addTableData(myLibrary) {
         if (i % 2 === 1) {
             newRow.style.backgroundColor = "lightgray";
         }
-        i++;
         let newCell = newRow.insertCell(-1);
         newCell.innerHTML = obj.title;
         newCell = newRow.insertCell(-1);
@@ -35,12 +36,17 @@ function addTableData(myLibrary) {
         newCell = newRow.insertCell(-1);
         newCell.innerHTML = obj.publicationYear;
         newCell = newRow.insertCell(-1);
-        if (obj.isRead) {
-            newCell.innerHTML = '<svg fill="#000000" width="50px" height="50px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>checkmark2</title><path d="M28.998 8.531l-2.134-2.134c-0.394-0.393-1.030-0.393-1.423 0l-12.795 12.795-6.086-6.13c-0.393-0.393-1.029-0.393-1.423 0l-2.134 2.134c-0.393 0.394-0.393 1.030 0 1.423l8.924 8.984c0.393 0.393 1.030 0.393 1.423 0l15.648-15.649c0.393-0.392 0.393-1.030 0-1.423z"></path></svg>';
-        }
-        else {
-            newCell.innerHTML = '<svg fill="#000000" width="50px" height="50px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>cancel2</title><path d="M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z"></path></svg>';
-        }
+        newCell.setAttribute('data-index', i);
+        newCell.innerHTML = obj.isRead ? check : x;
+        i++;
+
+        newCell.addEventListener('click', (e) => {
+            // closest to allow clicking on icon
+            const bookIndex = e.target.closest('td').getAttribute('data-index'); 
+            // toggle isRead
+            myLibrary[bookIndex].isRead = !myLibrary[bookIndex].isRead; 
+            addTableData(myLibrary);
+        })
     }
 }
 
@@ -53,6 +59,7 @@ function addBookToLibrary() {
 myLibrary.push(new Book('Bionic Commando', 'F.X. Nine', '117', '1991', true), new Book('Star Wreck', 'Leah Rewolinski', '117', '1990', false), new Book('Bored of the Rings', 'The Harvard Lampoon', '149', '1969', true));
 
 addTableData(myLibrary);
+
 
 newBtn.addEventListener("click", () => newDialog.showModal());
 
