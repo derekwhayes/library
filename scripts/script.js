b3e54e259c2f5d8c1a1ff1d7327c70a0052359f9
Derek Hayes <derek2099@gmail.com>
2024-10-10 08:34:11 -0500

    Bootstrap some sample books for testing

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 const myLibrary = [];
 
-function Book(title) {
+function Book(title, author, publicationYear) {
     this.title = title;
+    this.author = author;
+    this.publicationYear = publicationYear;
 }
 
 function addBookToLibrary() {
     title = prompt('What is the title of your book? ');
     myLibrary.push(new Book(title));
 }
+
+myLibrary.push(new Book('Bionic Commando', 'F.X. Nine', '1991'), new Book('Star Wreck', 'Leah Rewolinski', '1990'), new Book('Bored of the Rings', 'The Harvard Lampoon', '1969'));
